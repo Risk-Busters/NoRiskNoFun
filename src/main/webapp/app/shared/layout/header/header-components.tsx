0b2aa4ff8b3b7479311bac0e5420d5cea51880f1
Nils Krehl <nils.krehl@lherk.de>
2019-11-20 22:02:52 +0100

    NRNF-43 changed main colors

--- src/main/webapp/app/shared/layout/header/header-components.tsx
+++ src/main/webapp/app/shared/layout/header/header-components.tsx
@@ -1,21 +1,21 @@
 import React from 'react';
-import { Translate } from 'react-jhipster';
+import {Translate} from 'react-jhipster';
 
-import { NavItem, NavLink, NavbarBrand } from 'reactstrap';
-import { NavLink as Link } from 'react-router-dom';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {NavbarBrand, NavItem, NavLink} from 'reactstrap';
+import {NavLink as Link} from 'react-router-dom';
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
 
 import appConfig from 'app/config/constants';
 
 export const BrandIcon = props => (
   <div {...props} className="brand-icon">
-    <img src="content/images/logo-jhipster.png" alt="Logo" />
+    <img src="content/images/TODO.svg" alt="Logo"/>
   </div>
 );
 
 export const Brand = props => (
   <NavbarBrand tag={Link} to="/" className="brand-logo">
-    <BrandIcon />
+    <BrandIcon/>
     <span className="brand-title">
       <Translate contentKey="global.title">NoRiskNoFun</Translate>
     </span>
@@ -26,7 +26,7 @@ export const Brand = props => (
 export const Home = props => (
   <NavItem>
     <NavLink tag={Link} to="/" className="d-flex align-items-center">
-      <FontAwesomeIcon icon="home" />
+      <FontAwesomeIcon icon="home"/>
       <span>
         <Translate contentKey="global.menu.home">Home</Translate>
       </span>
@@ -37,7 +37,7 @@ export const Home = props => (
 export const Projects = props => (
   <NavItem>
     <NavLink tag={Link} to="/entity/project" className="d-flex align-items-center">
-      <FontAwesomeIcon icon="object-group" />
+      <FontAwesomeIcon icon="object-group"/>
       <span>
         <Translate contentKey="global.menu.projects"/>
       </span>

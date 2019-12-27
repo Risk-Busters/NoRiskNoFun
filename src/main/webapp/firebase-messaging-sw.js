8a541607572c8ca2611aaf3c3e5d7f4922177618
Yanamoe <moritz.horch@web.de>
2019-12-27 09:31:28 +0100

    NRNF-52 ServiceWorker hard to implement with given style guide, ignore

--- src/main/webapp/firebase-messaging-sw.js
+++ src/main/webapp/firebase-messaging-sw.js
@@ -1,2 +1,4 @@
 importScripts('https://www.gstatic.com/firebasejs/3.9.0/firebase-app.js');
 importScripts('https://www.gstatic.com/firebasejs/3.9.0/firebase-messaging.js');
+
+// TODO: Implement Service Worker for FCM here

fcbe2c938127f2f8c589f56313c5117ffe390164
Nils Krehl <nils.krehl@lherk.de>
2020-01-03 11:38:57 +0100

    NRNF-68 add achievemnts type enum

---
+++ src/main/java/com/riskbusters/norisknofun/domain/enumeration/AchievementType.java
@@ -0,0 +1,8 @@
+package com.riskbusters.norisknofun.domain.enumeration;
+
+/**
+ * The ProbabilityType enumeration.
+ */
+public enum AchievementType {
+    SURE, PROBABLY, MAYBE, NOTLIKELY, NOTGONNAHAPPEN
+}
